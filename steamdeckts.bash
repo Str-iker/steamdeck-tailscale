33e6cc42444ea48e3fb0699b3b953acf84e4fd2d
Striker <keystriker03@gmail.com>
2022-08-24 22:35:54 -0400

    change paths to home

--- steamdeckts.bash
+++ steamdeckts.bash
@@ -2,24 +2,23 @@
 ts=tailscale_1.28.0_amd64
 curl https://pkgs.tailscale.com/stable/$ts.tgz > /tmp/tailscale.tgz
 tar xf /tmp/tailscale.tgz -C /tmp/
-mv /tmp/$ts /tmp/tailscale
+mv /tmp/$ts ~/tailscale
 
-tee << EOF >> /tmp/tailscale-service.desktop
+tee << EOF >> ~/tailscale/tailscale-service.desktop
 [Desktop Entry]
 Encoding=UTF-8
 Version=1.0
 Type=Application
 Terminal=true
-Exec=konsole -e /tmp/tailscale/tailscaled -tun userspace-networking -socket /tmp/tailscaled.sock
-Name=Tailscale Service
+Exec=konsole -e ~/tailscale/tailscaled -tun userspace-networking -socket /tmp/tailscaled.sock
 EOF
 
-tee << EOF >> /tmp/tailscale-client.desktop
+tee << EOF >> ~/tailscale/tailscale-client.desktop
 [Desktop Entry]
 Encoding=UTF-8
 Version=1.0
 Type=Application
 Terminal=true
-Exec=konsole -e /tmp/tailscale/tailscale -socket /tmp/tailscaled.sock up
+Exec=konsole -e ~/tailscale/tailscale -socket /tmp/tailscaled.sock up
 Name=Tailscale Client
 EOF
